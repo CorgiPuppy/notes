db234c7c09c2c4435b52e31c01d58f2c05004b2e
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-11-18 17:51:26 +0300

    feat: create a script for writing daily results.

---
+++ heavenly-food/writeBibleChapters.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+declare FOLDER="daily-practices/2025/11-november/"
+declare FILE=results.csv
+
+cd $FOLDER
+
+echo -n "Old testament: "
+read amount chapters
+echo -n "\"$(date +"%d.%m.%Y")\",$amount,$chapters," >> $FILE
+
+echo -n "New testament: "
+read amount chapters
+echo "$amount,$chapters,,,,,," >> $FILE
+
+echo -n "Commit and Push?: "
+read answer
+if [[ "$answer" =~ ^([yY][eE][sS]|[yY])$ ]]
+then
+	git add $FILE
+	git commit -m  "feat: add results for today."
+	git push origin master
+fi
