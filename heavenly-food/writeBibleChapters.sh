05f953bb4abf0729306794a3543628b7f7467f81
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2025-12-08 11:35:26 +0300

    fix: update.

--- heavenly-food/writeBibleChapters.sh
+++ heavenly-food/writeBibleChapters.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+month=$(date +"%m")
+echo $month
 declare FOLDER="daily-practices/2025/11-november/"
 declare FILE=results.csv
 
@@ -6,11 +8,11 @@ cd $FOLDER
 
 echo -n "Old testament: "
 read amount chapters
-echo -n "\"$(date +"%d.%m.%Y")\",$amount,\"$chapters\"," >> $FILE
+echo -n "\"$(date +"%d.%m.%Y")\",$amount,$chapters," >> $FILE
 
 echo -n "New testament: "
 read amount chapters
-echo "$amount,\"$chapter\"s,,,,,," >> $FILE
+echo "$amount,$chapters,,,,,," >> $FILE
 
 echo -n "Commit and Push?: "
 read answer
