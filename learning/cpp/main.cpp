3deb0df51853e7a974a3f618508b0c7db875e38b
CorgiPuppy <andrey.zolotukhin2017@mail.ru>
2026-08-08 00:24:03 +0300

    feat: add some methods for my learning.

--- learning/cpp/main.cpp
+++ learning/cpp/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <time.h>
+#include <string>
 
 void introduction() {
 	/*
@@ -44,13 +46,195 @@ void workWithTypeDatas() {
 }
 
 void workWithConditions() {
+	int password = 1234;
+	std::cout << "Запускаю while" << std::endl;
+	while (true) {
+		int pincode;
+		char command;
+		std::cout << "To exit enter the 'e' and 'o' to write the pincode" << std::endl;
+		std::cout << "Command: ";
+		std::cin >> command;
+		switch (command) {
+			case 'o':
+				std::cout << "Захожу в if, если не вышли" << std::endl;
+				std::cout << "Enter the pincode: ";
+				std::cin >> pincode;
+				if (password == pincode) {
+					std::cout << "Succeed!" << std::endl;
+					return;
+				} else {
+					std::cout << "Unsucceed!" << std::endl;
+					std::cout << "выхожу и пинкода" << std::endl;
+				}			
+				break;
+			case 'e':
+				return;
+			default:
+				std::cout << "введите нужную букву" << std::endl;
+				break;
+		}
+		/*
+		if (command == 'o') {
+			std::cout << "Захожу в if, если не вышли" << std::endl;
+			std::cout << "Enter the pincode: ";
+			std::cin >> pincode;
+			if (password == pincode) {
+				std::cout << "Succeed!" << std::endl;
+				return;
+			} else {
+				std::cout << "Unsucceed!" << std::endl;
+				std::cout << "выхожу и пинкода" << std::endl;
+			}
+		} else if (command == 'e')
+			return;
+		else
+			std::cout << "введите нужную букву" << std::endl;
+		*/
+	}
+}
+
+void doCalculator() {
+	srand(time(NULL));
+	int range[2] = {-20, 0};	
+	int numberOne = range[0] + rand() % (range[1] - range[0] + 1);
+	int numberTwo = range[0] + rand() % (range[1] - range[0] + 1);
+	std::cout << "The first number: " << numberOne << ". and the second number: " << numberTwo << "." << std::endl;
+	while (true) {
+		char operation;
+		std::cout << "You can write the operation you want to do or 'e' to exit: ";
+		std::cin >> operation;
+		switch (operation) {
+			case '+':
+				std::cout << numberOne << " " << operation << " " << numberTwo << '=' << numberOne + numberTwo << std::endl;
+				break;
+			case '-':
+				std::cout << numberOne << " " << operation << " " << numberTwo << '=' << numberOne - numberTwo << std::endl;
+				break;
+			case '*':
+				std::cout << numberOne << " " << operation << " " << numberTwo << '=' << numberOne * numberTwo << std::endl;
+				break;
+			case '/':
+				std::cout << numberOne << " " << operation << " " << numberTwo << '=' << numberOne / numberTwo << std::endl;
+				break;
+			case '%':
+				std::cout << numberOne << " " << operation << " " << numberTwo << '=' << numberOne % numberTwo << std::endl;
+				break;
+			case 'e':
+				std::cout << "The end of the program.." << std::endl;
+				return;
+			default:
+				std::cout << "Enter the right symbol!" << std::endl;
+				break;
+				}
+				}
+}
+
+void suggestNumber() {
+	int range[2] = {-5, 5};
+	int realNumber = range[0] + rand() % (range[1] - range[0] + 1);
+	while (true) {
+		int suggestedNumber;
+		std::cout << "Suggest the number: ";
+		std::cin >> suggestedNumber;
+		if (suggestedNumber == realNumber) {
+			std::cout << "Huuuuray!! The right number is " << realNumber << std::endl;	
+			break;
+		}
+		else {
+			std::cout << "Try again!" << std::endl;	
+			continue;
+		}
+	}
+}
+
+void workWithArrays() {
+	short evenNumbers[3];
+	evenNumbers[0] = 0;
+	evenNumbers[1] = 2;
+	evenNumbers[2] = 4;
+
+	int lenOfOddNumbers = 4;
+	float oddNumbers[lenOfOddNumbers] = {1.4f, 2.f, 5.13f};
+
+	for (int i = 0; i < lenOfOddNumbers; i++)
+		std::cout << "oddNumbers[" << i << "] = " << oddNumbers[i] << std::endl;
 	
+	int lengthOfArray;
+	std::cout << "Enter the length of an array: ";
+	std::cin >> lengthOfArray;
+	int numbers[lengthOfArray];
+	int number;
+	for (int i = 0; i < lengthOfArray; i++) {
+		std::cout << "Enter the number of the array: ";
+		std::cin >> number;
+		numbers[i] = number;
+	}
+	int sumOfElements = 0;
+	for (int i = 0; i < lengthOfArray; i++) {
+		sumOfElements += numbers[i];
+	}
+	int minimumOfElements = numbers[0];
+	for (int i = 0; i < lengthOfArray; i++)
+		if (numbers[i] < minimumOfElements)
+			minimumOfElements = numbers[i];
+	for (int i = 0; i < lengthOfArray; i++)
+		std::cout << "numbers[" << i << "] = " << numbers[i] << std::endl;
+	std::cout << "Сумма элементов массива = " << sumOfElements << std::endl;
+	std::cout << "Минимальный элемент массива = " << minimumOfElements << std::endl;
+
+	int matrix[3][2] = {
+		{2, -3},
+		{-2, 5},
+		{2, -4}
+	};
+	for (int i = 0; i < 3; i++) {
+		for (int j = 0; j < 2; j++)
+			std::cout << matrix[i][j] << " ";
+		std::cout << std::endl;
+	}
+}
+
+void workWithDynamicArrays() {
+	int *nums = new int[3];
+	nums[0] = 45;
+	nums[1] = 23;
+	for (int i = 0; i < 3; i++)
+		std::cout << nums[i] << std::endl;
+	delete[] nums;
+
+	for (int i = 0; i < 3; i++)
+		std::cout << nums[i] << std::endl;
+}
+
+void workWithStrings() {
+	char word[] = {'H', 'e', 'l', 'l', 'o'};
+	for (int i = 0; i < 5; i++) {
+		std::cout << word[i];
+	}
+	std::cout << '\n';
+
+	std::string words = "Hello, my World!";
+	std::cout << words << std::endl;
+	words[6] = '-';
+	std::cout << words << std::endl;
+
+	std::cin >> words;
+	std::cout << words << std::endl;
+}
+
+int doSum(int a, int b) {
+	return a + b;
+}
+
+void workWithFunctions() {
+	std::cout << "Sum of two numbers: " << doSum(5, 20) << std::endl;
 }
 
 int main() {
 	setlocale(LC_ALL, "ru_RU.UTF-8");
+	srand(time(NULL));
 
-	workWithConditions();
+	workWithFunctions();
 
     return 0;
 }
